142478ff57ede9a66079ed885629487240e8ba49
hansent <thomas.hansen@gmail.com>
2015-03-28 12:51:55 -0500

    add mongolab/mongoose

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
+var mongoose = require ("mongoose"); 
 var express = require('express');
 var app = express();
+
+
+mongoose.connect(process.env.MONGOLAB_URI, function (err, res) {
+  if (err) { 
+    console.log ('ERROR connecting to: ' + process.env.MONGOLAB_URI + '. ' + err);
+  } else {
+    console.log ('Succeeded connected to: ' + process.env.MONGOLAB_URI);
+  }
+});
+
+
+var User = mongoose.model('User', { 
+    phone: String,
+    seat: String,
+    messages: [{ 
+        text: String, 
+        time: { type: Date, default: Date.now },
+        done: Boolean 
+    }],
+
+});
+
+
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
+app.post('/api/post_message', function(request, response) {
   response.send('Hello World!');
 });
 
